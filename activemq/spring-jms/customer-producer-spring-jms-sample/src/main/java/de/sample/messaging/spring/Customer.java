69e7ee8f66e1ee328a119184c2840e1b78be6408
Lorenz <lorenz04@gmx.net>
2022-04-13 10:27:26 +0200

    erstellen von producer

---
+++ activemq/spring-jms/customer-producer-spring-jms-sample/src/main/java/de/sample/messaging/spring/Customer.java
@@ -0,0 +1,37 @@
+package de.sample.messaging.spring;
+
+import java.time.LocalDate;
+
+public class Customer {
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private LocalDate birthdate;
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public LocalDate getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(LocalDate birthdate) {
+		this.birthdate = birthdate;
+	}
+
+}
---
+++ activemq/spring-jms/customer-producer-spring-jms-sample/src/main/java/de/sample/messaging/spring/CustomersController.java
@@ -0,0 +1,49 @@
+package de.sample.messaging.spring;
+
+import java.util.Collection;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("customers")
+public class CustomersController {
+
+	private final CustomersService service;
+	private final CustomersJMSProducer producer;
+
+	public CustomersController(CustomersService service, CustomersJMSProducer producer) {
+		super();
+		this.service = service;
+		this.producer = producer;
+	}
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public Collection<Customer> getCustomers() {
+		return this.service.getCustomers();
+	}
+
+	@GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Customer getCustomer(@PathVariable("id") long id) {
+		return this.service.findById(id).orElseThrow(NotFoundException::new);
+	}
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Void> sendCustomer(@RequestBody Customer customer) {
+		this.service.addCustomer(customer);
+		this.producer.send(customer);
+		var locationHeader = linkTo(methodOn(CustomersController.class).getCustomer(customer.getId())).toUri(); // HATEOAS
+		return ResponseEntity.created(locationHeader).build();
+	}
+
+}
